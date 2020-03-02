7303041dd9c4d138c8d7f61df627e56c306777db
Alessandro Nesi <alexandronesi@gmail.com>
2020-03-02 19:04:14 +0100

    initialized mvn project

---
+++ src/test/java/com/a29340/shoppinglist/ShoppingListApplicationTests.java
@@ -0,0 +1,14 @@
+package com.a29340.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
