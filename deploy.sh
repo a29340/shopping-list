c8cf8b07389d403506e8bf747f0d719341d06580
a29340 <k3UqEL6pD7nhYH6>
2020-03-26 22:53:53 +0100

    updated build and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+mvn clean install
 scp target/shopping-list-0.0.1-SNAPSHOT.jar root@192.168.1.123:/home/orangepi/application
 echo "systemctl restart shoppinglist" | ssh root@192.168.1.123
