553524b891b92b15f568819e7b3fc78845078893
alexandronesi@gmail.com <46le54si>
2020-04-16 19:03:27 +0200

    updated deployDev.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mvn clean install
-scp target/shopping-list-0.0.1-SNAPSHOT.jar root@192.168.1.123:/home/orangepi/application
-echo "systemctl restart shoppinglist" | ssh root@192.168.1.123
+scp target/shopping-list-0.0.1-SNAPSHOT.jar orangepi@192.168.1.123:/home/orangepi/application
+echo "systemctl restart shoppinglist" | ssh orangepi@192.168.1.123
