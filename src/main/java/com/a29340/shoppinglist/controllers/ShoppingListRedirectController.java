00d18f148899bfb55021105c9503dbe20b5b8d1f
alexandronesi@gmail.com <46le54si>
2020-04-22 00:22:58 +0200

    wip on forward

--- src/main/java/com/a29340/shoppinglist/controllers/ShoppingListRedirectController.java
+++ src/main/java/com/a29340/shoppinglist/controllers/ShoppingListRedirectController.java
@@ -2,11 +2,17 @@ package com.a29340.shoppinglist.controllers;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ShoppingListRedirectController implements ErrorController {
 
   public static final String ERROR_PATH = "/error";
+
+  @RequestMapping(ERROR_PATH)
+  String redirect(){
+    return "forward:index.html";
+  }
 
   @Override
   public String getErrorPath() {
