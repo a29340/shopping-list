8e3fc7f068ce6ee8e27c5d7c284973c2b3f03347
alexandronesi@gmail.com <46le54si>
2020-06-08 21:20:02 +0200

    updated env

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ cd ../shopping-list-fe
 ng build --prod
 cd ../shopping-list
 mvn clean install
-scp target/shopping-list-0.0.1-SNAPSHOT.jar root@192.168.1.123:/home/orangepi/application
-echo "systemctl restart shoppinglist" | ssh root@192.168.1.123
+scp target/shopping-list-0.0.1-SNAPSHOT.jar orangepiwin@192.168.1.126:/home/orangepiwin/application/
+echo "sudo systemctl restart shoppinglist" | ssh orangepiwin@192.168.1.126
--- deployDev.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mvn clean install
-scp target/shopping-list-0.0.1-SNAPSHOT.jar root@192.168.1.123:/home/orangepi/application/dev
-echo "systemctl restart shoppinglist-dev" | ssh root@192.168.1.123
